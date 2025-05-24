e83a4f617cf47a834f99221554cc14b87cc6afe7
Kanish Jain <jainkanish412@gmail.com>
2025-05-24 09:09:16 +0530

    Finally

--- client/client-side/src/Preview.jsx
+++ client/client-side/src/Preview.jsx
@@ -13,7 +13,7 @@ function Preview({ file, clearFile }) {
   }
   return (
     <div className="previewDiv">
-      <img alt="Thumbnail" src="./src/assets/file-icon.png" /> 
+      <img alt="Thumbnail" src="https://th.bing.com/th/id/OIP.wYlyKcT2kYHALFkxd8A0BwHaHa?cb=iwp2&rs=1&pid=ImgDetMain" /> 
       <p className="fileName">{file.name}</p>
       <p className="fileSize">({formatFileSize(file.size)})</p>
       <button onClick={removeFile}>❌</button>
